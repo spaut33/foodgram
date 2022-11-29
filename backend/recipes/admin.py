4bdb6189a526a34ff332f9109ab466a017488779
spaut33 <development@telfia.com>
2022-11-30 01:38:35 +0300

    admin sites for recipes and small fix for users

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,41 @@
-from django.contrib import admin
+from datetime import datetime
 
-# Register your models here.
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from recipes.models import Ingredient, Tag, Unit, Recipe, RecipeIngredient
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    list_editable = ('color',)
+
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    list_editable = ('name',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('measurement_unit',)
+    list_editable = ('measurement_unit',)
+
+
+class RecipeIngredientInLine(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 2
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'text', 'cooking_time', 'user', 'date_added')
+    readonly_fields = ('date_added',)
+    inlines = (RecipeIngredientInLine,)
