4cafd42e8e408cb59fe7e33e4e1eb3f0d09e2f84
Roman Petrakov <development@telfia.com>
2022-12-10 15:52:49 +0300

    infra fixes, backend Dockerfile

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,8 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='django-secret-key')
 DEBUG = os.getenv('DEBUG', default=False) == 'True'
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split()
 
 # Application definition
 
